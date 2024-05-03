27046f7f0cc02eb4c4141aebfef580e6c687dc67
Iris <93379906+Iris-LPB@users.noreply.github.com>
2024-05-03 10:59:04 +0200

    Created my first triangle

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,23 @@ int main()
     // Initialisation
     gl::init("TPs de Rendering"); // On crée une fenêtre et on choisit son nom
     gl::maximize_window(); // On peut la maximiser si on veut
+    auto const triangle_mesh = gl::Mesh{{
+        .vertex_buffers = {{
+            .layout = {gl::VertexAttribute::Position2D{0}},
+            .data   = {
+                -1.f, -1.f,    
+                +1.f, -1.f,
+                0.f, +1.f
+            }
+        }},
+    }};
 
     while (gl::window_is_open())
     {
         // Rendu à chaque frame
+        glClearColor(0.f, 0.f, 1.f, 1.f); // Choisis la couleur à utiliser. Les paramètres sont R, G, B, A avec des valeurs qui vont de 0 à 1
+        glClear(GL_COLOR_BUFFER_BIT); // Exécute concrètement l'action d'appliquer sur tout l'écran la couleur choisie au-dessus
+        gl::bind_default_shader(),
+        triangle_mesh.draw();
     }
 }
