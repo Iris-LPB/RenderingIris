fa2ba9691d701f19594a14f3f583d3fd37f35dad
Iris <93379906+Iris-LPB@users.noreply.github.com>
2024-05-03 12:02:46 +0200

    Turned triangle to rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,15 @@ int main()
         .vertex_buffers = {{
             .layout = {gl::VertexAttribute::Position2D{0}},
             .data   = {
-                -1.f, -1.f,    
-                +1.f, -1.f,
-                0.f, +1.f
+                -0.5f, -0.5f,    
+                -0.5f, 0.5f,
+                0.5f, 0.5f,
+
+                -0.5f, -0.5f,    
+                0.5f, -0.5f,
+                0.5f, 0.5f
+
+
             }
         }},
     }};
@@ -21,7 +27,7 @@ int main()
         // Rendu à chaque frame
         glClearColor(0.f, 0.f, 1.f, 1.f); // Choisis la couleur à utiliser. Les paramètres sont R, G, B, A avec des valeurs qui vont de 0 à 1
         glClear(GL_COLOR_BUFFER_BIT); // Exécute concrètement l'action d'appliquer sur tout l'écran la couleur choisie au-dessus
-        gl::bind_default_shader(),
+        gl::bind_default_shader();
         triangle_mesh.draw();
     }
 }
