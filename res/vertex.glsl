148aa062c9c7b352466de10f7f128534c54d4d94
Iris <93379906+Iris-LPB@users.noreply.github.com>
2024-07-14 15:13:05 +0200

    Modified vertex shader to render the rectangle slanted and with an offset
    this is fun!

--- res/vertex.glsl
+++ res/vertex.glsl
@@ -5,5 +5,8 @@ layout(location = 0) in vec2 in_position;
 
 void main()
 {
-    gl_Position = vec4(in_position, 0., 1.);
+    vec2 position = in_position;
+    position.x = position.x + position.y*0.10 + 0.35;
+    position.y = position.y + 0.25;
+    gl_Position = vec4(position, 0., 1.);
 }
